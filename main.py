14caa943c8b0dca6461388843359c806ffcd4ade
student <yakimmm1983@yandex.ru>
2023-10-05 21:02:44 +0300

    add task from the lesson

---
+++ main.py
@@ -0,0 +1,50 @@
+
+
+
+
+def Vibor():
+    figura = str(input("Выберите фигуру: прямоугольник,круг,трапеция,треугольник  "))
+    raschet = str(input("Что вы хотите расчитать: площадь,периметр фигуры  "))
+    if raschet == "площадь":
+        print(ploshad(figura))
+    if raschet == "периметр":
+        print(perimetr(figura))
+
+def ploshad(figura):
+    while True:
+        if figura == "прямоугольник":
+            pr = int(input("введите сторону а: "))
+            pr1 = int(input("введите сторону b: "))
+            print(pr+pr+pr1+pr1)
+        if figura == "круг":
+            kr = int(input("введите радиус круга: "))
+            print(3.14*kr**2)
+        if figura == "трапеция":
+            tr1 = int(input("введите сторону а: "))
+            tr2 = int(input("введите сторону b: "))
+            tr3 = int(input("введите сторону h: "))
+            print(((tr1+tr2)*tr3)/2)
+        if figura == "треугольник":
+            treug1 = int(input("введите сторону а: "))
+            treug2 = int(input("введите сторону b: "))
+            print((treug1*treug2)/2)
+        
+def perimetr(figura):
+    while True:
+        if figura == "прямоугольник":
+            pr = int(input("введите сторону а: "))
+            pr1 = int(input("введите сторону b: "))
+            print(pr*pr1)
+        if figura == "круг":
+            kr = int(input("введите радиус круга: "))
+            print(2*3.14*kr)
+        if figura == "трапеция":
+            tr1 = int(input("введите сторону а: "))
+            tr2 = int(input("введите сторону b: "))
+            tr3 = int(input("введите сторону c: "))
+            tr4 = int(input("введите сторону d: "))
+            print(tr1+tr2+tr3+tr4)
+
+Vibor()
+
+
